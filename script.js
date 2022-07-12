2216477554d78c0447edfb4e6606ebe74dd040cd
ASKI17cii <aski17cii@gmail.com>
2022-07-13 07:59:31 +0900

    4th

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var today = new Date();
 var year = today.getFullYear(); // 년도
 var month = today.getMonth();  // 월
 
-document.querySelector('#cal').value = today.getFullYear() + "-" +(today.getMonth() + 1).toString().padStart(2,'0');
-
-
 var months = [30, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 
